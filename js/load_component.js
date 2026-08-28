78e006cfb5873b1213267bd6fbf2360fcc5e4e58
comp-tech-guy <allthought123@gmail.com>
2026-08-28 08:10:40 -0700

    Page Transition

--- js/load_component.js
+++ js/load_component.js
@@ -1,8 +1,15 @@
 function loadComponent(tagName, filePath) {
     // const element = document.getElementById(placeholderID);
     const element = document.querySelector(tagName);
-    if (!element) return;
-    
+  if (!element) return;
+
+  const cacheKey = `component_${filePath}`;
+      const cached = sessionStorage.getItem(cacheKey);
+      if (cached) {
+          element.innerHTML = cached;
+          return;
+      }
+
     fetch(filePath)
         .then(response => response.text())
         .then(response => {
